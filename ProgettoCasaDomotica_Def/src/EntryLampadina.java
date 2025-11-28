b636c5424e132908a3de3b2415b79fdf2516e632
Ricky <riccardorighi39@gmail.com>
2025-11-28 09:52:24 +0100

    Add files via upload

---
+++ ProgettoCasaDomotica_Def/src/EntryLampadina.java
@@ -0,0 +1,10 @@
+class EntryLampadina {
+    public int x, y;
+    public String stanza;
+
+    public EntryLampadina(int potenza, int x, String stanza) {
+        this.x = x;
+        this.y = y;
+        this.stanza = stanza;
+    }
+}
