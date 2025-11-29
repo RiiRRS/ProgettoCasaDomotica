edb833e3ac0339df455d42e7652ede7ac5e9775d
Sasha <warsnice69@gmail.com>
2025-11-29 20:13:43 +0100

    Add files via upload

---
+++ ProgettoCasaDomotica_Grafica/src/LampadinaIntelligente.java
@@ -0,0 +1,134 @@
+import java.io.Serializable;
+import graphics.*;
+
+public class LampadinaIntelligente implements Serializable {
+
+    private int potenza;
+    private int luminosita;    // valore tra 0 e 100
+    private String colore;
+    private String nome;
+    private boolean acceso;
+
+    private int x;
+    private int y;
+    private Ellipse cerchio;
+
+    public LampadinaIntelligente(int potenza){
+        if (potenza < 0){
+            potenza = Math.abs(potenza);
+        }
+        this.potenza = potenza;
+        this.nome = "";
+        this.luminosita = 50;
+        this.colore = "giallo";
+        this.acceso = false;
+    }
+
+    public void setPosizione(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void creaGrafica(Picture p) {
+        int r = 40;
+
+        cerchio = new Ellipse(x - r/2, y - r/2, r, r);
+        cerchio.setColor(Color.GRAY);
+        cerchio.draw();
+    }
+
+    public void aggiornaGrafica() {
+        if (cerchio == null) return;
+
+        Color c;
+
+        switch (colore.toLowerCase()) {
+            case "rosso": c = Color.RED; break;
+            case "verde": c = Color.GREEN; break;
+            case "blu":   c = Color.BLUE; break;
+            case "giallo": c = Color.YELLOW; break;
+            default: c = Color.WHITE; break;
+        }
+
+        if (!acceso) {
+            c = Color.GRAY;
+        }
+
+        int r = 20;
+
+        int glowLayers = luminosita / 20;
+
+        for (int i = 1; i <= glowLayers; i++) {
+            Ellipse glow = new Ellipse(
+                    x - (r/2 + i*3),
+                    y - (r/2 + i*3),
+                    r + i*6,
+                    r + i*6
+            );
+
+            glow.setColor(c);
+            glow.draw();
+        }
+
+        cerchio = new Ellipse(x - r/2, y - r/2, r, r);
+        cerchio.setColor(c);
+        cerchio.fill();
+    }
+
+
+    public int getPotenza(){
+        return this.potenza;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return this.nome;   // CORRETTO
+    }
+
+    public void setAcceso(boolean acceso) {
+        this.acceso = acceso;
+        aggiornaGrafica();
+    }
+
+    public boolean getAcceso(){
+        return this.acceso;
+    }
+
+    public int getLuminosita() {
+        return this.luminosita;
+    }
+
+    public void aumentaIlluminazione (){
+        if (luminosita < 100) {
+            luminosita += luminosita * 10 / 100;
+            if (luminosita > 100) luminosita = 100;
+        }
+    }
+
+    public void diminuisciIlluminazione (){
+        if (luminosita > 0) {
+            luminosita -= luminosita * 10 / 100;
+            if (luminosita < 0) luminosita = 0;
+        }
+    }
+
+    public void setColore(String colore) {
+        this.colore = colore;
+    }
+
+    public String getColore() {
+        return this.colore;   // CORRETTO
+    }
+
+    @Override
+    public String toString(){
+        return "Nome: " + getNome() +
+                " | Potenza: " + getPotenza() +
+                " | Stato: " + getAcceso() +
+                " | Illuminazione: " + getLuminosita() +
+                " | Colore: " + getColore();
+    }
+}
---
+++ ProgettoCasaDomotica_Grafica/src/LettoreCoordinate.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+class LettoreCoordinate {
+
+    public static ArrayList<EntryLampadina> leggiTutte() throws Exception {
+
+        ArrayList<EntryLampadina> lista = new ArrayList<>();
+
+        BufferedReader br = new BufferedReader(new FileReader("Assets/Coordinate.txt"));
+
+        String riga = br.readLine();
+
+        while (riga != null) {
+
+            String[] parti = riga.split(" ");
+
+            int x = Integer.parseInt(parti[0]);
+            int y = Integer.parseInt(parti[1]);
+            String stanza = parti[2];
+
+            lista.add(new EntryLampadina(50, x, y, stanza));
+
+            riga = br.readLine();  // legge la riga successiva
+        }
+
+        br.close();
+        return lista;
+    }
+}
