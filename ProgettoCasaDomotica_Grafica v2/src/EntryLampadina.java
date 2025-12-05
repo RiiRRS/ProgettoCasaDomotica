1c301718c9cc284f695be3ae3f2865c1404409d9
Sasha <warsnice69@gmail.com>
2025-12-05 09:35:11 +0100

    Add files via upload

---
+++ ProgettoCasaDomotica_Grafica v2/src/EntryLampadina.java
@@ -0,0 +1,15 @@
+import java.io.Serializable;
+
+class EntryLampadina implements Serializable {
+    public int potenza;
+    public int x;
+    public int y;
+    public String stanza;
+
+    public EntryLampadina(int potenza, int x, int y, String stanza) {
+        this.potenza = potenza;
+        this.x = x;
+        this.y = y;
+        this.stanza = stanza;
+    }
+}
